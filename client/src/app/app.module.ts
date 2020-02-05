80d52e465ec763d8853e1e82a2e832898d0a3c33
AlexWesterman <aw743@exeter.ac.uk>
2020-02-05 15:34:51 +0000

    Alex:   - Cleaned up css to use classes   - Initial creation of gamemaster-main view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerMainComponent } from './player-main/player-main.component';
+import { GamemasterMainComponent } from './gamemaster-main/gamemaster-main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerMainComponent
+    PlayerMainComponent,
+    GamemasterMainComponent
   ],
   imports: [
     BrowserModule,
