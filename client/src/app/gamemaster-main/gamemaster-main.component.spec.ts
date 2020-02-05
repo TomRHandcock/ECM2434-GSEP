8f1be07c4b729d2fc3891e637cd594a96cbd3584
AlexWesterman <aw743@exeter.ac.uk>
2020-02-05 16:21:28 +0000

    Alex:   - re-commit to add in gamemaster-main component

---
+++ client/src/app/gamemaster-main/gamemaster-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GamemasterMainComponent } from './gamemaster-main.component';
+
+describe('GamemasterMainComponent', () => {
+  let component: GamemasterMainComponent;
+  let fixture: ComponentFixture<GamemasterMainComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GamemasterMainComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GamemasterMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/gamemaster-main/gamemaster-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamemaster-main',
+  templateUrl: './gamemaster-main.component.html',
+  styleUrls: ['./gamemaster-main.component.scss']
+})
+export class GamemasterMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
