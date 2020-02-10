e6e8741564eed96372c01a84f733a70b594d56b9
Alex <aw743@exeter.ac.uk>
2020-02-10 14:16:51 +0000

    - Added in the nav bar for the gamemaster

--- src/app/gamemaster-main/gamemaster-main.component.ts
+++ src/app/gamemaster-main/gamemaster-main.component.ts
@@ -1,13 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-gamemaster-main',
   templateUrl: './gamemaster-main.component.html',
   styleUrls: ['./gamemaster-main.component.scss']
 })
-export class GamemasterMainComponent implements OnInit {
 
-  constructor() { }
+export class GamemasterMainComponent implements OnInit {
+  closeIcon = faArrowLeft;
+  menuIcon = faBars;
+
+  showMenu: boolean;
+  screen: string;
+
+  constructor() {
+    this.screen = 'overview';
+    this.showMenu = true;
+  }
+
+  /**
+   * Changes the screen
+   * @param screen - the screen to change to
+   * @author AlexWesterman
+   */
+  changeScreen(screen: string) {
+    if (!screen || !(screen === 'overview' || screen === 'team' || screen === 'question' || screen === 'QRcode')) {
+      throwError(screen + ' is not a valid screen type');
+    }
+
+    this.screen = screen;
+  }
+
+  /**
+   * Show or hide the navbar menu
+   * @author George White
+   */
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 
   ngOnInit() {
   }
