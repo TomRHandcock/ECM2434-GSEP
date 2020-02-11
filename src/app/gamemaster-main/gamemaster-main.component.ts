43dbe02039434982d15b5cbdbac2ccc663f78449
Owen Saunders <owen3mail@gmail.com>
2020-02-11 14:25:39 +0000

    Added QRCodeComponent to generate QR Codes

--- src/app/gamemaster-main/gamemaster-main.component.ts
+++ src/app/gamemaster-main/gamemaster-main.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gamemaster-main.component.scss']
 })
 export class GamemasterMainComponent implements OnInit {
+  qrComponent: QRCodeComponent = null;
+  myQrData: string = null;
+  constructor() {
+    // Debug: console.log("Reached GameMasterMain Constructor");
 
-  constructor() { }
+    // myQrData is shown on the Code
+    this.qrComponent = new QRCodeComponent();
+    this.myQrData = this.qrComponent.myQrData;
+   }
 
   ngOnInit() {
   }
 
+}
+
+/*
+ * This is the constructing component for QR Codes. A random number
+ * is created and used for the QR Code (changes upon refresh) which
+ * is then displayed on screen within <qrcode> html tags.
+ *
+ * @author OGWSaunders
+ */
+export class QRCodeComponent {
+  public myQrData = 'default';
+  randInteger: number = null;
+  constructor() {
+    // Debug: console.log("Reached QRCode Constructor");
+
+    // assign a random max. of 1 billion (9 digits)
+    this.randInteger = Math.floor(Math.random() * Math.floor(999999999));
+    this.myQrData = '[' + (this.randInteger).toString() + ']';
+  }
 }
