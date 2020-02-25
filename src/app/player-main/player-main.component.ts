3b1312649f8ae17e9b8861e6a3b6ddee2c368992
Owen Saunders <owen3mail@gmail.com>
2020-02-25 16:18:29 +0000

    Added QR Scanning in Player (By button)

--- src/app/player-main/player-main.component.ts
+++ src/app/player-main/player-main.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 import { faCamera, faGlobe, faBars, faHome, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import {QrScannerComponent} from 'angular2-qrscanner';
 
 @Component({
   selector: 'app-player-main',
   templateUrl: './player-main.component.html',
   styleUrls: ['./player-main.component.scss']
 })
-export class PlayerMainComponent implements OnInit {
+export class PlayerMainComponent {
   // Re-export Font Awesome icons for use in HTML
   scanQrCodeIcon = faCamera;
   visitWebsiteIcon = faGlobe;
@@ -21,8 +22,11 @@ export class PlayerMainComponent implements OnInit {
   score = 0;
 
   showMenu = false;
+  caseQR = null;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth) { }
+  @ViewChild(QrScannerComponent, {static: false}) qrScannerComponent !: QrScannerComponent;
+
+  constructor(private router: Router, private afAuth: AngularFireAuth, private renderer: Renderer2) { }
 
   /**
    * Sets the screen to the progress page
@@ -32,6 +36,42 @@ export class PlayerMainComponent implements OnInit {
    */
   openProgress() {
     this.screen = 'progress';
+  }
+
+  /**
+   * Sets the screen to the qrScanner page
+   * @author OGWSaunders
+   */
+  openQrScanner() {
+    this.screen = 'qrScanner';
+  }
+
+  openCamera() {
+    this.qrScannerComponent.getMediaDevices().then(devices => {
+      const videoDevices: MediaDeviceInfo[] = [];
+      for (const device of devices) {
+        if (device.kind.toString() === 'videoinput') {
+          videoDevices.push(device);
+        }
+      }
+      if (videoDevices.length > 0) {
+        let choosenDev;
+        for (const dev of videoDevices) {
+          if (dev.label.includes('front')) {
+            choosenDev = dev;
+            break;
+          }
+        }
+        if (choosenDev) {
+          this.qrScannerComponent.chooseCamera.next(choosenDev);
+        } else {
+          this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+        }
+      }
+    });
+    this.qrScannerComponent.capturedQr.subscribe(result => {
+      console.log(result);
+    });
   }
 
   /**
@@ -49,8 +89,4 @@ export class PlayerMainComponent implements OnInit {
   signOut() {
     this.afAuth.auth.signOut().then(() => this.router.navigate(['login']));
   }
-
-  ngOnInit() {
-  }
-
 }
