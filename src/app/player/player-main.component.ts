30254be590dcae8d47b16015286b7c4cb6e0af1e
George White <me@galexite.uk>
2020-03-08 08:58:36 +0000

    ng lint --fix

--- src/app/player/player-main.component.ts
+++ src/app/player/player-main.component.ts
@@ -116,7 +116,7 @@ export class PlayerMainComponent implements OnInit {
               isInTeam = true;
             }
           });
-        } catch(error) {}
+        } catch (error) {}
       });
 
       // End as not necessary
