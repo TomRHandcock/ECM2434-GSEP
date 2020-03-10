57851dc9d4305a536d5e7684bdade8a28f9bcea4
TomRHandcock <tomrhandcock@gmail.com>
2020-03-10 19:46:47 +0000

    Teams are now organised on the scoreboard and the Gamemaster team is not displayed.

--- src/app/gamemaster/scoreboard/scoreboard.component.ts
+++ src/app/gamemaster/scoreboard/scoreboard.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Team} from '../../database.schema';
+import { AngularFireDatabase } from '@angular/fire/database';
+import {ActivatedRoute, Router} from '@angular/router';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-scoreboard',
@@ -9,13 +12,38 @@ import {Team} from '../../database.schema';
 export class ScoreboardComponent implements OnInit {
 
   @Input() teams: Team[];
+  db;
+  gameId;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, db: AngularFireDatabase) {
+    this.db = db;
   }
 
   ngOnInit() {
+    // Get the game ID
+    const gameIdObservable = this.activatedRoute.paramMap.pipe(map(p => p.get('id')));
+    gameIdObservable.subscribe(id => {
+      this.gameId = id;
+    });
+    // Get an array of the teams
+    this.db.list('games/' + this.gameId + '/team').valueChanges().subscribe((teams) => {
+      // Remove the game masters' team
+      teams.forEach((element, index) => {
+        if(element.name == 'Gamemaster team') {
+          teams.splice(index,1);
+        }
+      });
+      // Sort the teams
+      this.teams = teams.sort(this.compare);
+    });
   }
 
+  /**
+   * The compare function for sorting the teams array on the scoreboard.
+   * @param a The first team
+   * @param b The second team
+   * @author TomRHandcock
+   */
   compare(a, b) {
     if (a.score > b.score) {
       return -1;
